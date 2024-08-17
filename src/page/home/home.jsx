28915789b8826a947b369a639a42b1b85498e090
Ahmet <ahmetarapaslan@gmail.com>
2024-08-17 22:22:33 +0300

    fix: footer

--- src/page/home/home.jsx
+++ src/page/home/home.jsx
@@ -17,17 +17,19 @@ import Tasarim from "../../png/tasarım.jpeg";
 import Tasarim1 from "../../png/tasarım1.jpeg";
 import Tasarim2 from "../../png/tasarım2.jpeg";
 import { Parallax, ParallaxProvider } from "react-scroll-parallax";
-import FooterVideo from "../../video/footerstock.mp4"
+import FooterVideo from "../../video/footerstock.mp4";
+import { FaInstagram } from "react-icons/fa6";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className="h-screen">
       <Header />
       <main className="relative w-full h-[calc(100%-100px)] rounded-lg mb-20">
-        <div className="absolute w-[calc(100%-15px)] mx-auto inset-0 bg-gradient-to-b from-black/35 to-transparent rounded-lg"></div>
+        <div className="absolute w-[calc(100%-10px)] mx-auto inset-0 bg-gradient-to-b from-black/35 to-transparent rounded-lg"></div>
         <img
           src={Klar}
-          className="w-[calc(100%-15px)] h-full mx-auto rounded-xl object-cover"
+          className="w-[calc(100%-10px)] h-full mx-auto rounded-xl object-cover"
         />
         <div className=" absolute top-[120px] left-1/2 -translate-x-1/2">
           <img width={300} src={KlarWhite} />
@@ -116,7 +118,7 @@ export default function Home() {
       <div className="relative ">
         <img
           src={ForYou}
-          className="w-[calc(100%-15px)] mx-auto rounded-xl h-[800px] object-cover"
+          className="w-full mx-auto rounded-xl h-[800px] object-cover"
         />
         <div className="absolute top-[100px] right-0 text-white w-[1000px] text-right pr-32">
           <h1 className="text-7xl">SİZİN İÇİN ÖZENLE TASARLANDI</h1>
@@ -185,20 +187,35 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <footer className="relative mt-20 h-[600px] w-[calc(100%-15px)] mx-auto ">
+      <footer className="relative mt-20 h-[800px]  ">
         <video
-          className="absolute top-0 left-0 w-full h-full object-cover rounded-xl"
+          className="absolute top-0 left-0 w-full h-full object-cover rounded-t-xl"
           src={FooterVideo}
           autoPlay
           loop
           muted
-          
         />
-                <div className="absolute w-[calc(100%-15px)] mx-auto inset-0 bg-gradient-to-b from-black/35 to-transparent rounded-lg"></div>
-
-        <div className="relative z-10 text-white p-10">
-          <h2 className="text-4xl font-bold">Footer Başlığı</h2>
-          <p className="mt-4 text-lg">Footer içeriği buraya gelecek.</p>
+        <div className="absolute  mx-auto inset-0 bg-gradient-to-t from-black/35 to-transparent rounded-lg"></div>
+        <div className=" absolute z-20 flex flex-col text-white items-center justify-center w-full h-full gap-y-20">
+          <h1 className="text-4xl font-semibold">
+            Hayalinizdeki Tasarım Parmaklarınızın Ucunda
+          </h1>
+          <a className="p-5 border-2 border-white text-lg font-semibold hover:bg-white hover:text-black duration-300 ">
+            Şimdi teklif alın
+          </a>
+        </div>
+        <div className=" absolute h-[80px] bg-black w-full bottom-0 flex justify-center ">
+          <div className="flex items-center gap-x-10 text-white">
+            <a href="#">Klar-Koltuk</a>
+            <a href="#">Hizmetler</a>
+            <a href="#">Konum</a>
+            <a href="#">İletişim</a>
+            <a href="#">İletişim</a>
+            <a className="flex items-center gap-x-5">
+              <FaInstagram size={20} />
+              <FaWhatsapp size={20} />
+            </a>
+          </div>
         </div>
       </footer>
     </div>
