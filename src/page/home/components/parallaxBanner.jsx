68baffac65bde9e5892ca09917cd2745f9bc1564
Ahmet <ahmetarapaslan@gmail.com>
2024-09-03 13:12:55 +0300

    fix:

--- src/page/home/components/parallaxBanner.jsx
+++ src/page/home/components/parallaxBanner.jsx
@@ -6,12 +6,12 @@ export default function ParallaxBanner({ title, title2 }) {
     <>
       <div className=" overflow-hidden">
         <Parallax translateX={[80, -80]}>
-          <h1 className="text-[60px] -mb-8 sm:text-[120px] font-montserrat sm:-mb-16 lg:text-[150px] xl:text-[200px] lg:-mb-24 xl:-mb-28 text-banner font-[900] tracking-widest text-stroke">
+          <h1 className="text-[60px] -mb-8 sm:text-[120px] font-montserrat sm:-mb-16 lg:text-[150px] xl:text-[200px] lg:-mb-24 xl:-mb-28 text-banner  tracking-widest text-stroke">
             {title}
           </h1>
         </Parallax>
         <Parallax translateX={[-80, 80]}>
-          <h1 className="text-[60px] sm:text-[120px] font-montserrat  lg:text-[150px] xl:text-[200px] text-banner font-[900] tracking-widest text-stroke">
+          <h1 className="text-[60px] sm:text-[120px] font-montserrat  lg:text-[150px] xl:text-[200px] text-banner  tracking-widest text-stroke">
             {title2}
           </h1>
         </Parallax>
